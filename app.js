0daa61b6616ab90ee9d688157c6ac9e437f6892a
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-12 13:00:40 +0530

    tours api GET setup

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
 const express = require ('express');
 const app =  express();
 
-app.get('/',(req,res)=>{
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours' , (req,res)=>{
+
+   res.status(200).json({
+       status:'success',
+       results:tours.length,
+       data:{
+           tours
+       }
+   })
+
+});
+
+/*app.get('/',(req,res)=>{
     //res.status(200).send('Hello from the server side');
     res.status(200).json({message:'Hello from the server side',app:'Natours'});
 
@@ -10,7 +25,7 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     res.send('You can post to this endpoint')
 })
-
+*/
 const port = 3000;
 app.listen(port,()=>{
     console.log(`App running on port ${port}`);
