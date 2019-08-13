4dd3aec61439f1ab3afa3a9b598f23d4664bf285
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-13 15:38:31 +0530

    POST a new tour to file

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
-const express = require ('express');
-const app =  express();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours' , (req,res)=>{
+app.get('/api/v1/tours', (req, res) => {
 
-   res.status(200).json({
-       status:'success',
-       results:tours.length,
-       data:{
-           tours
-       }
-   })
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    //  console.log(newTour);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+            
+        })  //201 status - created
+    });
 
 });
 
@@ -27,7 +47,7 @@ app.post('/',(req,res)=>{
 })
 */
 const port = 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App running on port ${port}`);
 })
 
