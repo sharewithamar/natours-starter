6b49b95ed932bc18e88221906d190db964d75fd2
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-14 11:48:02 +0530

    Get tour by id

--- app.js
+++ app.js
@@ -4,36 +4,59 @@ const app = express();
 
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
+//for optional parameter use ? - eg /:id?
+app.get('/api/v1/tours/:id', (req, res) => {
+  //console.log(req.params)
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
 
+  //if (id > tours.length) {
+    if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    //  console.log(newTour);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-            
-        })  //201 status - created
-    });
-
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  //  console.log(newTour);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      }); //201 status - created
+    }
+  );
 });
 
 /*app.get('/',(req,res)=>{
@@ -48,7 +71,5 @@ app.post('/',(req,res)=>{
 */
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
-
-
+  console.log(`App running on port ${port}`);
+});
