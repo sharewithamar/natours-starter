ae1ce1a6b2f911669af7d377c368170b17dd2522
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-21 16:25:23 +0530

    param  middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,89 +1,85 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`, typeof val);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id'
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-      status: 'success',
-      requestedAt: req.requestTime,
-      results: tours.length,
-      data: {
-        tours
-      }
-    });
-  };
-  
-  exports.getTour = (req, res) => {
-    //console.log(req.params)
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-  
-    //if (id > tours.length) {
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id'
-      });
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
     }
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour
-      }
-    });
-  };
-  
-  exports.createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    //  console.log(newTour);
-    tours.push(newTour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      err => {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            tour: newTour
-          }
-        }); //201 status - created
-      }
-    );
-  };
-  
-  exports.updateTour = (req, res) => {
-    // note just for learning express - acutal patch update on file not implemented
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id'
-      });
-    }
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: '<Updated new tour here>'
-      }
-    });
-  };
-  
-  exports.deleteTour = (req, res) => {
-    // note just for learning express - acutal patch update on file not implemented
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id'
-      });
-    }
-    //204 - no content status - sent for delete operations
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
-  };
+  });
+};
 
+exports.getTour = (req, res) => {
+  //console.log(req.params)
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  //if (id > tours.length) {
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+};
+
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  //  console.log(newTour);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      }); //201 status - created
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  // note just for learning express - acutal patch update on file not implemented
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated new tour here>'
+    }
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  // note just for learning express - acutal patch update on file not implemented
+
+  //204 - no content status - sent for delete operations
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {getAllTours,createTour,getTour,updateTour,deleteTour} = require('../controllers/tourController'); //destructuring
+const {
+  checkId,
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour
+} = require('../controllers/tourController'); //destructuring
 
+router.param('id', checkId);
 router
   .route('/')
   .get(getAllTours)
   .post(createTour);
-  router
+router
   .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
-  
-  module.exports = router ;
-    
 
+module.exports = router;
