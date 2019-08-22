cff53d03bb3a1b396c7a7057b71aa2d3b4849cca
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-22 12:20:58 +0530

    chaining middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,15 @@ exports.checkId = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Name or price information missing for the tour'
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
   checkId,
+  checkBody,
   getAllTours,
   createTour,
   getTour,
@@ -13,7 +14,7 @@ router.param('id', checkId);
 router
   .route('/')
   .get(getAllTours)
-  .post(createTour);
+  .post(checkBody, createTour);
 router
   .route('/:id')
   .get(getTour)
