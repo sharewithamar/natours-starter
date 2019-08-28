87374c80d6bf240cd3cb45320d85b9acc77969f8
Amarnath <sharewithamar@gmail.com>
2019-08-27 19:25:48 -0700

    saving a  document in model

--- server.js
+++ server.js
@@ -33,6 +33,38 @@ mongoose
     console.log('DB connection successful');
   });
  */
+
+// schema type options are optional. you can define simply name: String too
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, ' A tour must have a price']
+  }
+});
+
+//Always use uppercase on modelNames and modelVariables - equivalent to class
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: ' The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => console.log('ERROR', err));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
