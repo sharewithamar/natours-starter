faed0c6bec155610da5170475c83e8b9e0a5572c
Amarnath <sharewithamar@gmail.com>
2019-08-27 18:45:21 -0700

    connecting to DB

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 /** config files should be read first before requiring app */
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 const app = require('./app');
 
+/**connecting to remote DB */
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(con => {
+    //console.log(con.connections);
+    console.log('DB connection successful');
+  });
+
+/**connect to local DB  */
+/* mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(con => {
+    //console.log(con.connections);
+    console.log('DB connection successful');
+  });
+ */
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
